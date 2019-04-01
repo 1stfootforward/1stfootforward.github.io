b6fa61045a95967b4f3ea2e9a6140e0574123d1b
olijames <olimjames@gmail.com>
2019-04-02 06:27:52 +1300

    Update organize.js

--- organise/js/organize.js
+++ organise/js/organize.js
@@ -625,18 +625,16 @@ function savePTBooking() {
 	var day = moment( $(".bookingpt-date-input").val() , "DD-MM-YYYY").format("d");
 	var dayCode = moment( $(".bookingpt-date-input").val() , "DD-MM-YYYY").format("ddd");
 	
-	var time =  $(".bookingpt-time-input").val();
-	var timeCode =  moment( $(".bookingpt-time-input").val() ).format("HH");
-	var timeCode2 =  moment( $(".bookingpt-time-input").val() ).format("MM");
-	var code = dayCode + timeCode + timeCode2 + "PT0";
+	var time =  $(".bookingpt-time-input").val().replace(':', '');
+	
+	var code = dayCode + time + "PT0";
 	var user = UserMaster[ $(".user-dropdown").val() ] ;
 
 	$( ".user-dropdown" ).each(function( index ) {
 	  if( $( this ).val() != "" ){
 	  	user =  UserMaster[ $( this ).val() ];
-	  	console.log(timeCode + timeCode2);
-	  	console.log(time);
-	  	BookingsMaster[BookingsMaster.length] = {status: 0, user: parseInt($(".user-dropdown").val()), displayuser: user.display,	classId: 0, 	code: code, type: "PT Session", 		time: parseInt(timeCode + timeCode2), 	displaytime: time, day: date, date: date, created_at: ""};
+
+	  	BookingsMaster[BookingsMaster.length] = {status: 0, user: parseInt($(".user-dropdown").val()), displayuser: user.display,	classId: 0, 	code: code, type: "PT Session", 		time: time, 	displaytime: time, day: date, date: date, created_at: ""};
 	  	postBooking(BookingsMaster.length - 1);
 	  }
 
